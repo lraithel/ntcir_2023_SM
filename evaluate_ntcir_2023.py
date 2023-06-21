ed6d8f1717a69a02dc9babfadcf11f2b8c0bb7aa
lraithel <lisa.raithel@googlemail.com>
2023-06-21 11:30:19 +0200

    added #TPs etc.

--- evaluate_ntcir_2023.py
+++ evaluate_ntcir_2023.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 from sklearn.metrics import balanced_accuracy_score
 from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 from sklearn.metrics import f1_score
-from sklearn.metrics import multilabel_confusion_matrix
 from sklearn.metrics import precision_recall_fscore_support
 from sklearn.metrics import recall_score
 
@@ -20,21 +20,32 @@ Authors: Hui-Syuan Yeh, Lisa Raithel
 """
 
 
-def confusion_matrix():
-    """..."""
-
-    """
-    y_true = np.array([[1, 0, 1],
-                       [0, 1, 0]])
-    y_pred = np.array([[1, 0, 0],
-                       [0, 1, 1]])
-    multilabel_confusion_matrix(y_true, y_pred)
-    """
-    pass
-
-
 def convert_to_binary(row):
     if sum(row) >= 1:
+        return 1
+    return 0
+
+
+def count_tns(row):
+    if row["binary_gold"] == 0 and row["binary_pred"] == 0:
+        return 1
+    return 0
+
+
+def count_tps(row):
+    if row["binary_gold"] == 1 and row["binary_pred"] == 1:
+        return 1
+    return 0
+
+
+def count_fps(row):
+    if row["binary_gold"] == 0 and row["binary_pred"] == 1:
+        return 1
+    return 0
+
+
+def count_fns(row):
+    if row["binary_gold"] == 1 and row["binary_pred"] == 0:
         return 1
     return 0
 
@@ -64,8 +75,12 @@ def get_per_class_scores(gold_df, pred_df):
     print(classification_report(golds, preds, target_names=classes, zero_division=0))
 
 
-def get_binary_scores(gold_df, pred_df):
-    """..."""
+def get_binary_scores(gold_df, pred_df, to_csv=True):
+    """Convert the gold and predicted classes to binary format and calculate scores.
+
+    Returns classification report and a CSV with counts for true positives (TPs),
+    false positives (FPs), true negatives (TNs) and false negatives (FNs).
+    """
 
     # create a separate dataframe for the content (not needed for scores)
     content_df = gold_df.filter(items=["train_id", "text"])
@@ -75,24 +90,35 @@ def get_binary_scores(gold_df, pred_df):
 
     # add a column with the binary label for each df
     gold_df["binary_gold"] = gold_df.apply(convert_to_binary, axis=1)
-    # print(gold_df)
     pred_df["binary_pred"] = pred_df.apply(convert_to_binary, axis=1)
 
-    # get only the labels
-    golds = gold_df.values.tolist()
-    preds = pred_df.values.tolist()
-
     # merge the "content" and the binary labels for a better analysis
-    # TODO: add #TPs, #FPs, #TN, #FN
     merged_binary_df = pd.concat(
         [content_df, gold_df["binary_gold"], pred_df["binary_pred"]], axis=1
     )
+
+    merged_binary_df["#TPs"] = merged_binary_df.apply(count_tps, axis=1)
+    merged_binary_df["#FPs"] = merged_binary_df.apply(count_fps, axis=1)
+    merged_binary_df["#TNs"] = merged_binary_df.apply(count_tns, axis=1)
+    merged_binary_df["#FNs"] = merged_binary_df.apply(count_fns, axis=1)
+
+    # get only the binary labels
+    golds = merged_binary_df["binary_gold"].values.tolist()
+    preds = merged_binary_df["binary_pred"].values.tolist()
+
+    # add total of counts
+    merged_binary_df.loc["Total"] = merged_binary_df.sum(numeric_only=True)
+
+    print(f"Confusion matrix:\n{confusion_matrix(golds, preds)}")
 
     print(
         classification_report(
             golds, preds, labels=[0, 1], target_names=["no ADR", "ADR"], zero_division=0
         )
     )
+
+    if to_csv:
+        merged_binary_df.to_csv("overview_predictions_binary.csv")
 
 
 def get_balanced_accuracy(gold_df, pred_df):
