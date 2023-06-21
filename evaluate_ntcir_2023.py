e16c7a30fb2922de6083acf69319b578e13f5c76
lraithel <lisa.raithel@googlemail.com>
2023-06-21 13:07:38 +0200

    added CSV output for per-class eval

--- evaluate_ntcir_2023.py
+++ evaluate_ntcir_2023.py
@@ -26,28 +26,68 @@ def convert_to_binary(row):
     return 0
 
 
-def count_tns(row):
+def count_tns_bin(row):
     if row["binary_gold"] == 0 and row["binary_pred"] == 0:
         return 1
     return 0
 
 
-def count_tps(row):
+def count_tps_bin(row):
     if row["binary_gold"] == 1 and row["binary_pred"] == 1:
         return 1
     return 0
 
 
-def count_fps(row):
+def count_fps_bin(row):
     if row["binary_gold"] == 0 and row["binary_pred"] == 1:
         return 1
     return 0
 
 
-def count_fns(row):
+def count_fns_bin(row):
     if row["binary_gold"] == 1 and row["binary_pred"] == 0:
         return 1
     return 0
+
+
+def count_tps(row, classes):
+    """..."""
+    tps = 0
+    for class_ in classes:
+        if row[class_ + "_gold"] == 1 and row[class_ + "_pred"] == 1:
+            tps += 1
+
+    return tps
+
+
+def count_fps(row, classes):
+    """..."""
+    fps = 0
+    for class_ in classes:
+        if row[class_ + "_gold"] == 0 and row[class_ + "_pred"] == 1:
+            fps += 1
+
+    return fps
+
+
+def count_tns(row, classes):
+    """..."""
+    tns = 0
+    for class_ in classes:
+        if row[class_ + "_gold"] == 0 and row[class_ + "_pred"] == 0:
+            tns += 1
+
+    return tns
+
+
+def count_fns(row, classes):
+    """..."""
+    fns = 0
+    for class_ in classes:
+        if row[class_ + "_gold"] == 1 and row[class_ + "_pred"] == 0:
+            fns += 1
+
+    return fns
 
 
 def load_data(file_name):
@@ -60,19 +100,37 @@ def drop_non_class_cols(df):
     return df.drop(columns=["train_id", "text"])
 
 
-def get_per_class_scores(gold_df, pred_df):
+def get_per_class_scores(gold_df, pred_df, to_csv=True):
     """..."""
+    content_df = gold_df.filter(items=["train_id", "text"])
+    # get rid of train ID and text for a cleaner overview
     gold_df = drop_non_class_cols(gold_df)
-    pred_df = drop_non_class_cols(pred_df)
-
     # get the class names
     classes = list(gold_df)
+    # add a suffix to distinguish the column headers from those of the preds
+    gold_df = gold_df.add_suffix("_gold")
+
+    pred_df = drop_non_class_cols(pred_df).add_suffix("_pred")
+
+    # Concatenate the gold and prediction dataframes.
+    # you can also add the IDs and texts for a better analysis:
+    # merged_df = pd.concat([content_df, gold_df, pred_df], axis=1)
+    merged_df = pd.concat([gold_df, pred_df], axis=1)
+
+    merged_df["#TPs"] = merged_df.apply(count_tps, axis=1, args=(classes,))
+    merged_df["#FPs"] = merged_df.apply(count_fps, axis=1, args=(classes,))
+    merged_df["#TNs"] = merged_df.apply(count_tns, axis=1, args=(classes,))
+    merged_df["#FNs"] = merged_df.apply(count_fns, axis=1, args=(classes,))
+    merged_df.loc["Total"] = merged_df.sum(numeric_only=True)
 
     # get only the labels
     golds = gold_df.values.tolist()
     preds = pred_df.values.tolist()
 
     print(classification_report(golds, preds, target_names=classes, zero_division=0))
+
+    if to_csv:
+        merged_df.to_csv("overview_predictions_per_class.csv")
 
 
 def get_binary_scores(gold_df, pred_df, to_csv=True):
@@ -97,10 +155,10 @@ def get_binary_scores(gold_df, pred_df, to_csv=True):
         [content_df, gold_df["binary_gold"], pred_df["binary_pred"]], axis=1
     )
 
-    merged_binary_df["#TPs"] = merged_binary_df.apply(count_tps, axis=1)
-    merged_binary_df["#FPs"] = merged_binary_df.apply(count_fps, axis=1)
-    merged_binary_df["#TNs"] = merged_binary_df.apply(count_tns, axis=1)
-    merged_binary_df["#FNs"] = merged_binary_df.apply(count_fns, axis=1)
+    merged_binary_df["#TPs"] = merged_binary_df.apply(count_tps_bin, axis=1)
+    merged_binary_df["#FPs"] = merged_binary_df.apply(count_fps_bin, axis=1)
+    merged_binary_df["#TNs"] = merged_binary_df.apply(count_tns_bin, axis=1)
+    merged_binary_df["#FNs"] = merged_binary_df.apply(count_fns_bin, axis=1)
 
     # get only the binary labels
     golds = merged_binary_df["binary_gold"].values.tolist()
@@ -162,7 +220,7 @@ def get_balanced_accuracy(gold_df, pred_df):
     print("acc_0: {}".format(acc_0 / len(golds)))
     print("acc_1: {}".format(acc_1_overall))
     print("balanced_acc: {}".format(balanced_acc / len(golds)))
-    print("exact_acc: {}".format(exact_match / len(golds)))
+    print("Percentage of matches (exact_acc): {}".format(exact_match / len(golds)))
     # balanced_acc is averaging, so if example has gold=[0,0,0], we only consider the acc_0
     # applying to the same logic, we only averaging accross the examples having 1
 
@@ -172,15 +230,21 @@ def main(gold_csv, pred_csv):
     gold_df = load_data(gold_csv)  # .values.tolist()
     pred_df = load_data(pred_csv)  # .values.tolist()
 
-    print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nBinary Scores:")
+    print(
+        "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nBinary Scores:\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    )
 
     get_binary_scores(gold_df, pred_df)
 
-    print("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nPer Class Scores:")
+    print(
+        "\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nPer Class Scores:\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    )
 
     get_per_class_scores(gold_df, pred_df)
 
-    print("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nBalanced Accuracy:")
+    print(
+        "\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nBalanced Accuracy:\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    )
     get_balanced_accuracy(gold_df, pred_df)
 
 
